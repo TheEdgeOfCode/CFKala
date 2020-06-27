49a9d08fe3728ca9eb3a09a5af54b6e6767e7cd5
Ali Hatami <hatam008@users.noreply.github.com>
2020-06-27 14:03:43 +0430

    initial commit CFKala Server Application

---
+++ cfkserver/src/main/java/com/codefathers/cfkserver/CfkserverApplication.java
@@ -0,0 +1,13 @@
+package com.codefathers.cfkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfkserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfkserverApplication.class, args);
+    }
+
+}
---
+++ cfkserver/src/test/java/com/codefathers/cfkserver/CfkserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefathers.cfkserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfkserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
