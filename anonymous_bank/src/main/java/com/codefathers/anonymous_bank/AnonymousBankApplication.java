9fa750a680a6523254e49289014c232e75af4968
Ali Hatami <hatam008@users.noreply.github.com>
2020-06-27 14:39:46 +0430

    initial commit Anonymous Bank Project Created

---
+++ anonymous_bank/src/main/java/com/codefathers/anonymous_bank/AnonymousBankApplication.java
@@ -0,0 +1,13 @@
+package com.codefathers.anonymous_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymousBankApplication.class, args);
+    }
+
+}
