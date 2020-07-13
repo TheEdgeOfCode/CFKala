4259f17988339e5f3a4aa2936b863fe283dd64a5
Ali Hatami <hatam008@users.noreply.github.com>
2020-07-14 03:39:06 +0430

    adding resources

--- client/src/main/java/com/codefathers/cfkclient/utils/Connector.java
+++ client/src/main/java/com/codefathers/cfkclient/utils/Connector.java
@@ -17,17 +17,20 @@ public class Connector {
     private RestTemplate restTemplate;
 
     public Connector(){
+        token = "";
         // TODO: 7/14/2020 fill token
         restTemplate = new RestTemplate();
     }
 
     private <T,U> ResponseEntity<U> post(String uri,T dto ,Class<U> type){
         HttpEntity<T> requestEntity = new HttpEntity<>(dto);
+        requestEntity.getHeaders().add("Authentication","cfk! " + token);
         return restTemplate.exchange(uri, POST,requestEntity,type);
     }
 
     public List<MiniProductDto> getAllProducts(FilterSortDto dto){
-        ResponseEntity<MiniProductArrayListDto> response = post("",dto, MiniProductArrayListDto.class);
+        ResponseEntity<MiniProductArrayListDto> response = post("127.0.0.1:8050/product/get_all_products",
+                dto, MiniProductArrayListDto.class);
         return response.getBody().getDtos(); // TODO: 7/14/2020 handle errors
     }
 }
