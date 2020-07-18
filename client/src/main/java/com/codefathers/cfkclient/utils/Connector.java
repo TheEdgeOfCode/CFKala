f38db634846458c80e5d4bf9452285e7e4434c81
sajad-paksima <59170401+sajad-paksima@users.noreply.github.com>
2020-07-19 03:37:13 +0430

    Connector completed!

--- client/src/main/java/com/codefathers/cfkclient/utils/Connector.java
+++ client/src/main/java/com/codefathers/cfkclient/utils/Connector.java
@@ -1,6 +1,7 @@
 package com.codefathers.cfkclient.utils;
 
 import com.codefathers.cfkclient.CacheData;
+import com.codefathers.cfkclient.dtos.bank.*;
 import com.codefathers.cfkclient.dtos.category.CategoryPM;
 import com.codefathers.cfkclient.dtos.category.CreateDTO;
 import com.codefathers.cfkclient.dtos.content.AdPM;
@@ -48,34 +49,34 @@ public class Connector {
         return connector;
     }
 
-    public Connector(){
+    public Connector() {
         token = "";
         restTemplate = new RestTemplate();
     }
 
-    private <T,U> ResponseEntity<U> post(String uri,T dto ,Class<U> type) throws Exception {
+    private <T, U> ResponseEntity<U> post(String uri, T dto, Class<U> type) throws Exception {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authentication","cfk! " + token);
-        HttpEntity<T> requestEntity = new HttpEntity<>(dto,headers);
+        headers.add("Authentication", "cfk! " + token);
+        HttpEntity<T> requestEntity = new HttpEntity<>(dto, headers);
         ResponseEntity<U> response = restTemplate.exchange(uri, POST, requestEntity, type);
-        if (response.getStatusCode().equals(HttpStatus.OK)){
+        if (response.getStatusCode().equals(HttpStatus.OK)) {
             return response;
-        }else {
+        } else {
             /*this might cause problem */
-            String error = response.getStatusCode().toString() +  response.getHeaders().get("ERROR");
+            String error = response.getStatusCode().toString() + response.getHeaders().get("ERROR");
             throw new Exception(error);
         }
     }
 
-    private <T,U> U get(String uri, T dto , Type type) throws Exception {
+    private <T, U> U get(String uri, T dto, Type type) throws Exception {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authentication","cfk! " + token);
-        HttpEntity<T> requestEntity = new HttpEntity<>(dto,headers);
+        headers.add("Authentication", "cfk! " + token);
+        HttpEntity<T> requestEntity = new HttpEntity<>(dto, headers);
         ResponseEntity<String> response = restTemplate.exchange(uri, GET, requestEntity, String.class);
-        if (response.getStatusCode().equals(HttpStatus.OK)){
+        if (response.getStatusCode().equals(HttpStatus.OK)) {
             Gson gson = new Gson();
-            return gson.fromJson(response.getBody(),type);
-        }else {
+            return gson.fromJson(response.getBody(), type);
+        } else {
             throw new Exception(new GsonJsonParser().parseMap(response.getBody()).get("error").toString());
         }
     }
@@ -87,11 +88,13 @@ public class Connector {
     }
 
     public List<MainContent> mainContents() throws Exception {
-        return get(address + "/content/get_main_contents",null,new TypeToken<ArrayList<MainContent>>(){}.getType());
+        return get(address + "/content/get_main_contents", null, new TypeToken<ArrayList<MainContent>>() {
+        }.getType());
     }
 
     public List<AdPM> getAds() throws Exception {
-        return get(address + "/content/all_ads",null,new TypeToken<ArrayList<AdPM>>(){}.getType());
+        return get(address + "/content/all_ads", null, new TypeToken<ArrayList<AdPM>>() {
+        }.getType());
     }
 
     public String login(LoginDto dto) throws Exception {
@@ -128,8 +131,8 @@ public class Connector {
         return Objects.requireNonNull(response.getBody());
     }
 
-    public void changeAmount(int productId,int amount) throws Exception {
-        post(address + "/customer/change_amount", "" + productId +  "," + amount, String.class);
+    public void changeAmount(int productId, int amount) throws Exception {
+        post(address + "/customer/change_amount", "" + productId + "," + amount, String.class);
     }
 
     public void deleteProductFromCart(Integer productId) throws Exception {
@@ -198,65 +201,69 @@ public class Connector {
     }
 
     public void editCategory(CategoryEditAttribute attribute) throws Exception {
-        post(address + "/category/edit",attribute,String.class);
+        post(address + "/category/edit", attribute, String.class);
     }
 
     public void removeCategory(Integer id) throws Exception {
-        post(address + "/category/remove",id,String.class);
+        post(address + "/category/remove", id, String.class);
     }
 
     public void addCategory(CreateDTO createDTO) throws Exception {
-        post(address + "/category/remove",createDTO,String.class);
+        post(address + "/category/remove", createDTO, String.class);
     }
 
     public ArrayList<String> getSpecialFeatureOfCategory(Integer id) throws Exception {
-        return get(address + "/category/get_special",id,new TypeToken<ArrayList<String>>(){}.getType());
+        return get(address + "/category/get_special", id, new TypeToken<ArrayList<String>>() {
+        }.getType());
     }
 
     public List<String> getPublicFeaturesOfCategory() throws Exception {
         return get(address + "/category/get_public", null,
-                new TypeToken<ArrayList<String>>(){}.getType());
+                new TypeToken<ArrayList<String>>() {
+                }.getType());
     }
 
     public ArrayList<CategoryPM> getAllCategories() throws Exception {
-        return get("",null,new TypeToken<ArrayList<CategoryPM>>(){}.getType());
+        return get("", null, new TypeToken<ArrayList<CategoryPM>>() {
+        }.getType());
     }
 
     public void addContent(String title, String content) throws Exception {
-        post(address + "/content/add_content",title + "~~~" + content,String.class);
+        post(address + "/content/add_content", title + "~~~" + content, String.class);
     }
 
     public void deleteContent(Integer id) throws Exception {
-        post(address + "/content/delete",id,String.class);
+        post(address + "/content/delete", id, String.class);
     }
 
     public void systematicDiscount(CreateDiscountSystematic createDiscount) throws Exception {
-        post(address + "/discount/systematic",createDiscount,String.class);
+        post(address + "/discount/systematic", createDiscount, String.class);
     }
 
     public void removeDiscountCode(String discountCode) throws Exception {
-        post(address + "/discount/delete",discountCode,String.class);
+        post(address + "/discount/delete", discountCode, String.class);
     }
 
     public void editDiscountCode(DiscountCodeEditAttributes attributes) throws Exception {
-        post(address + "/discount/edit",attributes,String.class);
+        post(address + "/discount/edit", attributes, String.class);
     }
 
     public void createDiscount(CreateDiscount dto) throws Exception {
-        post(address + "/discount/create",dto,String.class);
+        post(address + "/discount/create", dto, String.class);
     }
 
     public void removeUserFromDiscountCodeUsers(String code, String username) throws Exception {
-        post(address + "/discount/remove_user",code + "~~~" + username,String.class);
+        post(address + "/discount/remove_user", code + "~~~" + username, String.class);
     }
 
     public void addUserToDiscountCode(AddUser dto) throws Exception {
-        post(address + "/discount/add_user",dto,String.class);
+        post(address + "/discount/add_user", dto, String.class);
     }
 
     public ArrayList<DisCodeManagerPM> getDiscountCodes() throws Exception {
-        return get(address + "/discount/get_discounts",null,
-                new TypeToken<ArrayList<DisCodeManagerPM>>(){}.getType());
+        return get(address + "/discount/get_discounts", null,
+                new TypeToken<ArrayList<DisCodeManagerPM>>() {
+                }.getType());
     }
 
     public void addOff(CreateOffDTO dto) throws Exception {
@@ -277,11 +284,11 @@ public class Connector {
     }
 
     public Image userImage(String text) throws Exception {
-        ByteArrayResource image = get(address + "/download/user/profile/" + text,null,
+        ByteArrayResource image = get(address + "/download/user/profile/" + text, null,
                 ByteArrayResource.class);
         if (image != null) {
             return new Image(new ByteArrayInputStream(image.getInputStream().readAllBytes()));
-        }else {
+        } else {
             return null;
         }
     }
@@ -324,29 +331,32 @@ public class Connector {
     }
 
     public List<MicroProductDto> sellerMicroProduct(String seller) throws Exception {
-        return get(address + "/seller/ad/micro/" + seller,null,
-                new TypeToken<ArrayList<MicroProductDto>>(){}.getType());
+        return get(address + "/seller/ad/micro/" + seller, null,
+                new TypeToken<ArrayList<MicroProductDto>>() {
+                }.getType());
     }
 
 
     public void addAd(CreateAd ad) throws Exception {
-        post(address + "/content/add_ad",ad,String.class);
+        post(address + "/content/add_ad", ad, String.class);
     }
 
     public int createCompany(String[] info) throws Exception {
-        ResponseEntity<Integer> response = post(address + "",info,Integer.class);
+        ResponseEntity<Integer> response = post(address + "", info, Integer.class);
         return Objects.requireNonNull(response.getBody());
     }
 
     public ArrayList<MessagePM> getMessagesForUser() throws Exception {
-        return get(address + "/messages/get_all",null,
-                new TypeToken<ArrayList<MessagePM>>(){}.getType());
+        return get(address + "/messages/get_all", null,
+                new TypeToken<ArrayList<MessagePM>>() {
+                }.getType());
     }
 
     public void openMessage(int id) {
         try {
-            post(address + "/messages/open/" + id,null,String.class);
-        } catch (Exception ignore) {}
+            post(address + "/messages/open/" + id, null, String.class);
+        } catch (Exception ignore) {
+        }
     }
 
     public void saveUserImage(InputStream stream) throws Exception {
@@ -354,8 +364,8 @@ public class Connector {
         post(address + "/upload/user/profile", resource, String.class);
     }
 
-    public void updateProductMainImage(int id,InputStream[] streams) throws Exception {
-        post(address + "/upload/product/" + id,streams,String.class);
+    public void updateProductMainImage(int id, InputStream[] streams) throws Exception {
+        post(address + "/upload/product/" + id, streams, String.class);
     }
 
     public Image productMainImage(int id) throws Exception {
@@ -366,15 +376,17 @@ public class Connector {
 
     public ArrayList<Image> loadImages(int id) throws Exception {
         ArrayList<Image> images = new ArrayList<>();
-        ArrayList<ByteArrayResource> resources = get(address + "/download/product/" + id,null,
-                new TypeToken<ArrayList<ByteArrayResource>>(){}.getType());
+        ArrayList<ByteArrayResource> resources = get(address + "/download/product/" + id, null,
+                new TypeToken<ArrayList<ByteArrayResource>>() {
+                }.getType());
         resources.forEach(byteArrayResource -> {
             try {
                 Image image = createImageFromResource(byteArrayResource);
                 if (image != null) {
                     images.add(image);
                 }
-            } catch (IOException ignore) {}
+            } catch (IOException ignore) {
+            }
         });
         return images;
     }
@@ -382,46 +394,50 @@ public class Connector {
     private Image createImageFromResource(ByteArrayResource resource) throws IOException {
         if (resource != null) {
             return new Image(new ByteArrayInputStream(resource.getInputStream().readAllBytes()));
-        }else {
+        } else {
             return null;
         }
     }
 
     public ArrayList<MicroProductDto> similarNameProducts(String name) throws Exception {
-        return get(address + "/product/similar/" + name,null,
-                new TypeToken<ArrayList<MicroProductDto>>(){}.getType());
+        return get(address + "/product/similar/" + name, null,
+                new TypeToken<ArrayList<MicroProductDto>>() {
+                }.getType());
     }
 
-    public void editProduct(ProductEditAttribute attribute) throws Exception{
-        post(address + "/products/edit",attribute,String.class);
+    public void editProduct(ProductEditAttribute attribute) throws Exception {
+        post(address + "/products/edit", attribute, String.class);
     }
 
     public FullProductPM viewAttributes(int id) throws Exception {
-        return get(address + "/products/full/" + id,null,FullProductPM.class);
+        return get(address + "/products/full/" + id, null, FullProductPM.class);
     }
 
     public List<CommentPM> viewProductComments(int id) throws Exception {
-        return get(address + "/product/comments/" + id,null,
-                new TypeToken<ArrayList<CommentPM>>(){}.getType());
+        return get(address + "/product/comments/" + id, null,
+                new TypeToken<ArrayList<CommentPM>>() {
+                }.getType());
     }
 
     public void addToCart(String[] info) throws Exception {
-        post(address + "/product/add_to_cart",info,String.class);
+        post(address + "/product/add_to_cart", info, String.class);
     }
 
     public void assignComment(String[] info) throws Exception {
-        post(address + "/product/comment/add",info,String.class);
+        post(address + "/product/comment/add", info, String.class);
     }
 
     public List<OffProductPM> showAllOnOffProducts(FilterSortDto filter) throws Exception {
-        return get(address + "products/on_off",filter,
-                new TypeToken<ArrayList<OffProductPM>>(){}.getType());
+        return get(address + "products/on_off", filter,
+                new TypeToken<ArrayList<OffProductPM>>() {
+                }.getType());
     }
 
-    public void logout(){
+    public void logout() {
         try {
-            post(address + "/users/logout",null,String.class);
-        } catch (Exception ignore) {}
+            post(address + "/users/logout", null, String.class);
+        } catch (Exception ignore) {
+        }
     }
 
     public List<OffDTO> viewAllOffs() throws Exception {
@@ -431,7 +447,41 @@ public class Connector {
 
     public boolean isTheFirstManager() {
         try {
-            return get(address + "/manager/is_first",null,Boolean.class);
-        } catch (Exception ignore) {return false;}
+            return get(address + "/manager/is_first", null, Boolean.class);
+        } catch (Exception ignore) {
+            return false;
+        }
+    }
+
+    public String createBankAccount(CreateBankAccountDTO dto) throws Exception {
+        ResponseEntity<String> response = post("http://127.0.0.1:8050/bank/create_account", dto, String.class);
+        return Objects.requireNonNull(response.getBody());
+    }
+
+    public String getToken(TokenRequestDTO dto) throws Exception {
+        ResponseEntity<String> response = get("http://127.0.0.1:8050/bank/get_token", dto, String.class);
+        return Objects.requireNonNull(response.getBody());
+    }
+
+    public int createReceipt(CreateReceiptDTO dto) throws Exception {
+        ResponseEntity<Integer> response = post("http://127.0.0.1:8050/bank/create_receipt",
+                dto, Integer.class);
+        return Objects.requireNonNull(response.getBody());
+    }
+
+    private List<TransactionDTO> getTransactions(NeededForTransactionDTO dto) throws Exception {
+        ResponseEntity<TransactionListDTO> response = get("http://127.0.0.1:8050/bank/get_transactions",
+                dto, TransactionListDTO.class);
+        return Objects.requireNonNull(response.getBody()).getDtos();
+    }
+
+    public void pay(String receiptId) throws Exception {
+        post("http://127.0.0.1:8050/bank/pay", receiptId, String.class);
+    }
+
+    public long getBalance(BalanceDTO dto) throws Exception {
+        ResponseEntity<Long> response = get("http://127.0.0.1:8050/bank/get_balance",
+                dto, Long.class);
+        return Objects.requireNonNull(response.getBody());
     }
 }
