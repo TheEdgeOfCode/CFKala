f0a62a83e34b872295c8810df3f42bcd06fd41b1
Ali Hatami <hatam008@users.noreply.github.com>
2020-07-21 22:33:26 +0430

    merge

--- client/src/main/java/com/codefathers/cfkclient/controllers/ManagerAccount.java
+++ client/src/main/java/com/codefathers/cfkclient/controllers/ManagerAccount.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ManagerAccount extends BackAbleController {
+
     @FXML private JFXButton addSupportButt;
     @FXML private JFXButton back;
     @FXML private JFXButton minimize;
